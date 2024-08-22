3a3ec659ffcc014197bd038c877102962b2df568
algiz-z <miyako.3214.4601@gmail.com>
2024-08-22 15:00:01 +0900

    add directories

---
+++ lambda/users-get-internship.js
@@ -0,0 +1,37 @@
+const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
+const client = new DynamoDBClient({ region: "ap-northeast-1" });
+const TableName = "User";
+
+exports.handler = async (event, context) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({ message: "" }),
+  };
+
+  //TODO: 取得対象のテーブル名をparamに宣言
+  const param = {};
+
+  const command = new ScanCommand(param);
+
+  try {
+    // client.send()で全件取得するコマンドを実行
+    const users = (await client.send(command)).Items;
+
+    //TODO: 全ユーザのpasswordを隠蔽する処理を記述
+
+    //TODO: レスポンスボディを設定する
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "予期せぬエラーが発生しました。",
+      errorDetail: e.toString(),
+    });
+  }
+
+  return response;
+};
---
+++ src/assets/config.js
@@ -0,0 +1,2 @@
+// 自分のエンドポイントに置き換えてください
+export const baseUrl = 'https://w278xet73i.execute-api.ap-northeast-1.amazonaws.com';
